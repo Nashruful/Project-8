ea1ae96d5d29983a5849d7b5e377c2965a994ad8
Amjad Elahi <amjadelahi07@gmail.com>
2024-09-28 19:40:17 +0300

    implements custom widget of app bar, container and list

---
+++ lib/components/lists/custom_orders_list_tile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../custom_text/custom_text.dart';
+
+class CustomOrdersListTile extends StatelessWidget {
+  const CustomOrdersListTile(
+      {super.key, required this.text, this.onPressed, this.icon, this.color});
+  final String text;
+  final Function()? onPressed;
+  final IconData? icon;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      leading: Icon(
+        icon,
+        color: color,
+      ),
+      title: CustomText(
+        text: text,
+        color: const Color(0xffD7D1CA),
+        size: 20,
+        weight: FontWeight.normal,
+      ),
+      trailing: IconButton(
+          onPressed: onPressed,
+          icon: const Icon(
+            Icons.arrow_forward_ios_outlined,
+            color: Color(0xffD7D1CA),
+          )),
+    );
+  }
+}

efe72ef0d8f3a4936e0d1151b8a4999c6ad0f836
Nashruful <azooz.n40@hotmail.com>
2024-09-30 09:49:29 +0300

    before merging my own main

--- lib/screens/auth/create_account_screen.dart
+++ lib/screens/auth/create_account_screen.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:onze_cofe_project/components/containers/custom_background_container.dart';
-import 'package:onze_cofe_project/components/custom_elevated_button/custom_elevated_button.dart';
 import 'package:onze_cofe_project/components/custom_text/custom_text.dart';
 import 'package:onze_cofe_project/components/text_form_field/custom_text_form_field.dart';
 
@@ -15,7 +13,9 @@ class CreateAccountScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Image.asset("assets/images/pattren 1.png"),
+            Image.asset(
+              "assets/images/pattren 1.png",
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(

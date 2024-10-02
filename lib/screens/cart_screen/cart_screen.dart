668b312144230e01e7f1c82d2e6704ef99637ec7
dev <nooof-2017a@hotmail.com>
2024-10-02 14:56:59 +0300

    create folder notification

--- lib/screens/cart_screen/cart_screen.dart
+++ lib/screens/cart_screen/cart_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:onze_cofe_project/components/items/cart_Item.dart';
-import 'package:onze_cofe_project/screens/order_notification_screen.dart';
+import 'package:onze_cofe_project/screens/notification_screen/order_notification_screen.dart';
 
 
 import 'cubit/cart_cubit.dart';

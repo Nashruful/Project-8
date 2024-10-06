33306aaaf591229f58736f3314d84ed7972d66ef
Abdualaziz Alruwaily <43842557+Nashruful@users.noreply.github.com>
2024-10-06 21:05:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: "https://atlxmjlltfikucaizoeg.supabase.co",
+    url: "nop",
     anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0bHhtamxsdGZpa3VjYWl6b2VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczNzkyNDMsImV4cCI6MjA0Mjk1NTI0M30.tdRENvJ39QV3yoTiEDTagQr7xQ3k-sHENQxwGGEuvF8",
+        "nuh uh",
   );
 
   await setup();
@@ -21,7 +21,7 @@ void main() async {
   ));
   OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
 
-  OneSignal.initialize("ebdec5c2-30a4-447d-9577-a1c13b6d553e");
+  OneSignal.initialize(":p");
 
   OneSignal.Notifications.requestPermission(true);
 }
